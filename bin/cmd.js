9568af98230737dd852d4f0e32f5aa3172cd965b
Steve Terpe <srterpe@gmail.com>
2016-04-08 02:51:52 -0700

    Rewrite without dependencies and dynamically figure the tabbing

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,8 +1,28 @@
 #!/usr/bin/env node
 
-var args = require('minimist')(process.argv.slice(2))
-var clear = require('cli-clear')
-var eighty = require('../')
+'use strict'
 
-clear()
-console.log(eighty)
+var eighty = require('../')
+var win = process.platform.indexOf('win') === 0
+var stdout = []
+var crlf = '\r\n'
+var lf = '\n'
+var i = 0
+var lines
+
+// https://github.com/stevenvachon/cli-clear
+
+if (!win) {
+  stdout.push('\x1B[2J')
+} else {
+  lines = process.stdout.getWindowSize()[1]
+  for(; i < lines; i++) {
+    stdout.push(crlf)
+  }
+}
+
+stdout.push('\x1B[0f')
+stdout.push(eighty)
+stdout.push(win ? crlf : lf)
+
+process.stdout.write(stdout.join(''))
--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-module.exports = [
-  '      +8     +16     +24     +32     +40',
-  '     +48     +56     +64     +72     +80'
-].join('')
+'use strict'
+
+var e = 'Tab-size too small'
+
+function writeTicks (tab, columns) {
+  var j = Math.floor(columns / tab)
+  var i = 1
+  var s = ""
+  var col
+  var spaces
+
+  j += 1
+  for (; i < j; ++i) {
+    col = i * tab
+    spaces = tab - 1 - col.toString().length
+    if (spaces < 0) {
+      throw new Error(e)
+    }
+    while (spaces--) {
+      s += " "
+    }
+    s += "+"
+    s += col.toString()
+  }
+  return s
+}
+
+module.exports = writeTicks(8, 80)
